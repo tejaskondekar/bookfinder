cedc29358c6257d202d279edf87300f1f8c0e397
tejaskondekar <65647121+tejaskondekar@users.noreply.github.com>
2021-01-19 19:33:09 +0530

    Add files via upload

---
+++ sports_event_management1/src/main/java/net/javaguides/springboot/SportsEventManagement1Application.java
@@ -0,0 +1,13 @@
+package net.javaguides.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SportsEventManagement1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SportsEventManagement1Application.class, args);
+	}
+
+}
